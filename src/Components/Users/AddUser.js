e6dece177a74fa647fc1ca6508b878e965278bd0
vikassinghrathor <rathorevikas2809@gmail.com>
2023-11-30 13:09:53 +0530

    Add College Name Field and Changes output color also

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -6,6 +6,7 @@ import Card from '../UI/Card';
   const AddUser = (props) => {
     const [users, setUsers] = useState([]);
     const [username, setUsername] = useState('');
+    const[collegename,setCollegeName] = useState('');
     const [age, setAge] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
     const addUserHandler = (event) => {
@@ -15,15 +16,19 @@ import Card from '../UI/Card';
   function handleUsernameChange(event) {
     setUsername(event.target.value);
   }
+  function handlecollegenameChange(event) {
+    setCollegeName(event.target.value);
+  }
 
   function handleAgeChange(event) {
     setAge(event.target.value);
   }
 
   function handleSubmit() {
-    if (username.trim() && age.trim() && parseInt(age) >= 0) {
-      setUsers([...users, { username: username.trim(), age: age.trim() }]);
+    if (username.trim() && age.trim() && collegename.trim() && parseInt(age) >= 0) {
+      setUsers([...users, { username: username.trim(), age: age.trim() , collegename: collegename.trim() }]);
       setUsername('');
+      setCollegeName('');
       setAge('');
       setErrorMessage('');
     } else if(age < 1 ) {
@@ -40,18 +45,20 @@ import Card from '../UI/Card';
       <form onSubmit={addUserHandler}>
         <label>Username</label>
         <input type="text" value={username} onChange={handleUsernameChange} />
+        <label>College Name</label>
+        <input type="text" value={collegename} onChange={handlecollegenameChange} />
         <label>Age</label>
         <input type='number' value={age} onChange={handleAgeChange} />
         <Button onClick={handleSubmit}>Add User</Button>
         {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
-        </form>
+      </form>
         </Card>
       </div>
       <div>
         <ul>
           {users.map((user, index) => (
-            <li key={index}>
-              {`Username: ${user.username}, Age: ${user.age}`}
+            <li key={index} style={{color: '#FFFFFF'}}>
+              {`Username: ${user.username}, Age: ${user.age} , CollegeName: ${user.collegename}`}
             </li>
           ))}
         </ul>
@@ -61,3 +68,4 @@ import Card from '../UI/Card';
 }
 
 export default AddUser;
+
